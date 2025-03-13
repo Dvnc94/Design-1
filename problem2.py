a418c572539894a38abf6f67de4f3aa10ce4df28
Divyanshi Srivastava <divyanshisrivastava@Divyanshis-MBP.attlocal.net>
2025-03-13 16:54:51 -0700

    python

---
+++ problem2.py
@@ -0,0 +1,40 @@
+'''
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Three line explanation of solution in plain english : same implementation as in class
+
+// Your code here along with comments explaining your approach
+'''
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.stack[-1][1])))
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1][0]
+        return -1
+
+    def getMin(self) -> int:
+        if self.stack:
+            return self.stack[-1][1]
+        return -1  
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
